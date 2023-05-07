4afc1c9481e1cc91cab6c6dd55363fce25467131
Kamilplhh <Kamil_PLHH@wp.pl>
2023-05-07 17:43:18 +0200

    Project environment preparation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import '../css/app.css'; 
+import weatherapp from './components/weatherapp.vue';
+
+const app = createApp({});
+
+app.component('weatherapp', weatherapp);
+
+app.mount('#app');
---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather-app</title>
+    
+</head>
+<body>
+@vite('resources/js/app.js')
+
+    <body class="bg-blue-500">
+        <div id="app" class="flex justify-center pt-32">
+            <weatherapp></weatherapp>
+        </div>
+       
+    </body>
+    
+</body>
+</html>
