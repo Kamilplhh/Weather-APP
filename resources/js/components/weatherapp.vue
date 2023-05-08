ea3877fff83b073839a7dfb85abc0ef4ce722dfc
Kamilplhh <Kamil_PLHH@wp.pl>
2023-05-08 21:21:08 +0200

    Project done

--- resources/js/components/weatherapp.vue
+++ resources/js/components/weatherapp.vue
@@ -2,6 +2,7 @@
     <div class="text-white mb-8">
         <div class="places-input text-gray-800">
             <input v-model="location" @input="fetchdata" type="text" class="w-full">
+
         </div>
         <div class="weather-container font-sans w-128 max-w-lg overflow-hidden rounded-lg bg-gray-900 shadow-lg mt-4">
             <div class="current-weather flex items-center justify-between px-6 py-8">
@@ -10,8 +11,8 @@
                         <div class="text-6xl font-semibold">{{ currentTemp }}</div>
                     </div>
                     <div class="mx-5">
-                        <div class="font-semibold">{{ currentStatus }},<br>
-                            Wind: {{ currentWind }}</div>
+                        <div class="font-semibold">{{ currentStatus + ',' }} <br>
+                            {{ 'Wind: ' + currentWind }}</div>
                         <div>{{ location }}</div>
                     </div>
                 </div>
@@ -22,57 +23,68 @@
 
             <div class="future-weather text-sm bg-gray-800 px-6 py-8 overflow-hidden">
                 <div v-for="forecast in forecasts" class="flex items-center">
-                    <div class="w-1/6 text-lg text-gray-200">MON</div>
+                    <div class="w-1/6 text-lg text-gray-200">{{ 'Day: ' + forecast.day }}</div>
                     <div class="w-3/6 px-4 flex items-center">
-                        <div>Wind:</div>
-                        <div class="ml-3">{{ forecast.wind }}</div>
+                        <div class="ml-3">{{ 'Wind: ' + forecast.wind }}</div>
                     </div>
                     <div class="w-2/6 text-3xl text-center">
                         <div>{{ forecast.temperature }}</div>
                     </div>
                 </div>
             </div>
-            
+
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            this.fetchdata()
-        },
-        data() {
-            return {
-                location: ''
+export default {
+    mounted() {
+        this.fetchdata()
+    },
+    data() {
+        return {
+            currentTemp: '',
+            currentStatus: '',
+            currentWind: '',
+            forecasts: [],
+            location: 'Warsaw',
+        }
+    },
+    methods: {
+        time() {
+            var skycons = new Skycons({ 'color': 'white' });
+
+            const today = new Date();
+            const time = today.getHours();
+            let icon = '';
+            if (time > 8 && time < 20) {
+                icon = 'clear-day';
+            } else {
+                icon = 'clear-night';
             }
+
+            skycons.add('iconCurrent', icon)
+            skycons.play()
         },
-        methods: {
-            fetchdata() {
-                var skycons = new Skycons({'color': 'white'});
-
-                const today = new Date();
-                const time = today.getHours();
-                let icon = '';
-                if(time > 8 && time < 20) {
-                    icon = 'clear-day';
-                }else {
-                    icon = 'clear-night';
-                }
-
+        fetchdata() {
+            if(this.location.length > 4){
                 fetch('https://goweather.herokuapp.com/weather/' + this.location)
                 .then(response => response.json())
                 .then(data => {
-                    
-                    this.currentTemp = data.temperature,
-                    this.currentStatus = data.description,
-                    this.currentWind = data.wind,
-                    this.forecasts = data.forecast,
-                    
-                    skycons.add('iconCurrent', icon)
-                    skycons.play()
+                        this.currentTemp = data.temperature,
+                        this.currentStatus = data.description,
+                        this.currentWind = data.wind,
+                        this.forecasts = data.forecast,
+
+                        this.time()
                 })
-            },
-        }
+            }
+            else {
+                console.log('Input is too small');
+            }
+            
+        },
     }
+}
 </script>
